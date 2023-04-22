4af2a091f1ce72a342ea2d5b7452660244681d5f
madhuraurunkar <110885163+madhuraurunkar@users.noreply.github.com>
2023-04-23 00:54:06 +0530

    Add files via upload

---
+++ Emp.java
@@ -0,0 +1,73 @@
+
+package com.servlets;
+
+public class Emp {  
+
+
+private String product_name,name,bill_no,product_id,quantity,price,phone_no,total_price;  
+;  
+public String getbill_no() {  
+    return bill_no;  
+}  
+public void setbill_no(String bill_no) {  
+    this.bill_no = bill_no;  
+}  
+
+public String getproduct_id() {  
+    return product_id;  
+}  
+public void setproduct_id(String product_id) {  
+    this.product_id = product_id;  
+}  
+
+public String getproduct_name() {  
+    return product_name;  
+}  
+public void setproduct_name(String product_name) {  
+    this.product_name = product_name;  
+}  
+public String getquantity() {  
+    return quantity;  
+}  
+public void setquantity(String quantity) {  
+    this.quantity= quantity;  
+}  
+public String getprice() {  
+    return price;  
+}  
+public void setprice(String price) {  
+    this.price= price;  
+}  
+
+public String gettotal_price() {  
+    return total_price;  
+}  
+public void settotal_price(String total_price) {  
+    this.total_price = total_price;  
+}  
+public String getname() {  
+    return name;  
+}  
+public void setname(String name) {  
+    this.name = name;  
+}  
+public String getphone_no() {  
+    return phone_no;  
+}  
+public void setphone_no(String phone_no) {  
+    this.phone_no= phone_no;  
+}  
+}
+
+
+  
+
+		
+		
+	
+	
+	
+	
+
+	
+
---
+++ Product.java
@@ -0,0 +1,17 @@
+package arrayList;
+import java.util.ArrayList;
+
+public class Product {
+	
+	
+	private int id;
+	private String product_name;
+	private String price;
+	
+	
+	 
+	
+	
+	
+
+}
